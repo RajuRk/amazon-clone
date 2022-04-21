b92f8476d2454143d85ba59936d2a8e380d69b1d
RajuRk <45646335+RajuRk@users.noreply.github.com>
2022-04-21 22:44:36 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <Home/>
       </div>
     </Router>
   );
