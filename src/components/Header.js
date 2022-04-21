fe74f28dcf3363dbb0bccaefdced2508eb1a7086
RajuRk <45646335+RajuRk@users.noreply.github.com>
2022-04-21 22:47:00 +0530

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -22,22 +23,29 @@ function Header() {
       <div className="header__nav">
         <Link to="/login" className="header_link">
           <div className="header__option">
-            <span>Hello Raju</span>
-            <span>Sign in</span>
+            <span className="header__optionLineOne">Hello Raju</span>
+            <span class="header__optionLineTwo">Sign in</span>
           </div>
         </Link>
 
         <Link to="/" className="header_link">
           <div className="header__option">
-            <span>Return</span>
-            <span>& Orders</span>
+            <span className="header__optionLineOne">Return</span>
+            <span class="header__optionLineTwo">& Orders</span>
           </div>
         </Link>
 
         <Link to="/" className="header_link">
           <div className="header__option">
-            <span>Your</span>
-            <span>Prime</span>
+            <span className="header__optionLineOne">Your</span>
+            <span class="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className="header_link">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon/>
+            <span class="header__optionLineTwo header__basketCount">0</span>
           </div>
         </Link>
       </div>
